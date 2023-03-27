b8b4e166bc71fdb3e9eaff935a4afa311498b969
LeoMesce <l.mesce@contrader.it>
2023-03-27 11:04:05 +0200

    Initial commit

---
+++ src/main/java/com/example/annarita/AnnaritaApplication.java
@@ -0,0 +1,13 @@
+package com.example.annarita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnaritaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnaritaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/annarita/AnnaritaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.annarita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnaritaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
